07acafa1778a5ad39e99cd8121f0f6ecdede6647
chenhaijian <1216948100@qq.com>
2021-05-27 21:56:02 +0800

    增加了长按按钮脚本，可执行点击或长按操作 增加了长按按钮的测试脚本

---
+++ Assets/UnityEngine.UI/UI/MyCode/LongPressedButton.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class LongPressedButton : Selectable
+{
+    public int frames = 60;
+    public UnityEvent onClick = new UnityEvent();
+    public UnityEvent onLongPressBegin = new UnityEvent();
+    public UnityEvent onLongPress = new UnityEvent();
+    public UnityEvent onLongPressEnd = new UnityEvent();
+
+    private int timer = 0;
+    private bool isDown = false;
+    private bool isLongPress = false;
+    private bool hasLongPressStart = false;
+
+    public override void OnPointerDown(PointerEventData eventData)
+    {
+        if(!isDown && !isLongPress)
+        {
+            isDown = true;
+        }
+    }
+
+    public override void OnPointerUp(PointerEventData eventData)
+    {
+        if(!isLongPress)
+        {
+            EventSystem.current.SetSelectedGameObject(this.gameObject);
+            onClick.Invoke();
+        }
+        else
+        {
+            onLongPressEnd.Invoke();
+        }
+        isDown = false;
+        isLongPress = false;
+        hasLongPressStart = false;
+        timer = 0;
+    }
+
+    protected override void Start()
+    {
+        timer = 0;
+        isDown = false;
+        isLongPress = false;
+        hasLongPressStart = false;
+    }
+
+    void Update()
+    {
+        if(isDown)
+        {
+            timer++;
+            if(timer >= frames)
+            {
+                isLongPress = true;
+                if(!hasLongPressStart)
+                {
+                    hasLongPressStart = true;
+                    onLongPressBegin.Invoke();
+                }
+                onLongPress.Invoke();
+            }
+        }
+    }
+}
---
+++ Assets/UnityEngine.UI/UI/MyCode/LongPressedButtonTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LongPressedButtonTest : MonoBehaviour
+{
+    public void OnLongPressBegin()
+    {
+        print("开始长按");
+    }
+
+    public void OnLongPress()
+    {
+        print("正在长按");
+    }
+
+    public void OnLongPressEnd()
+    {
+        print("长按结束");
+    }
+
+    public void OnShortClick()
+    {
+        print("点击");
+    }
+}
