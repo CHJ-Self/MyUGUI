c2ea7fa9849870662532079b55636c6bef43c299
chenhaijian <chenhaijian@aobi.com>
2021-06-12 19:51:27 +0800

    提交透视滑动列表脚本（未完成）

--- Assets/UnityEngine.UI/UI/MyCode/PerspectiveImage.cs
+++ Assets/UnityEngine.UI/UI/MyCode/PerspectiveImage.cs
@@ -6,112 +6,115 @@ using UnityEngine.UI;
 
 public class PerspectiveImage : MonoBehaviour
 {
-    public float centerX = 0f;
-    public PosY posY = PosY.middle;
-    public float perspective = 0;
-    public float offset = 0;
-    [HideInInspector]
-    public Image[] images;
+    /// <summary>
+    /// 中心点，中心点可以设置为不是原点
+    /// </summary>
+    public Vector3 uiCenterPoint = Vector3.zero;
 
-    private RectTransform rectTransform;
+    /// <summary>
+    /// 变形的是哪边，up为上，down为下
+    /// </summary>
+    public PosY posY = PosY.up;
+
+    /// <summary>
+    /// 图片左边的回拉像素，每距离中心点100个像素往回拉的距离
+    /// </summary>
+    [Range(0,100)]
+    public float perspective_left = 0;
+
+    /// <summary>
+    /// 图片右边的回拉像素，每距离中心点100个像素往回拉的距离
+    /// </summary>
+    [Range(0, 100)]
+    public float perspective_right = 0;
+
+    /// <summary>
+    /// 滑动列表节点
+    /// </summary>
+    public RectTransform scrollRectRectTransform;
+
+    [HideInInspector]
+    private Image[] images;
+    
+
+    private List<RectTransform> childRectTransformList = new List<RectTransform>();
     private float _offset;
+    private Vector3[] fourConners= new Vector3[4];
+    private RectTransform rectTransform;
 
     private void Start()
     {
         images = GetComponentsInChildren<Image>();
+        rectTransform = GetComponent<RectTransform>();
+        for(int i = 0;i < transform.childCount;i++)
+        {
+            childRectTransformList.Add(transform.GetChild(i).GetComponent<RectTransform>());
+        }
     }
 
     public void ModifyMesh(VertexHelper vh)
     {
-        if (!gameObject.activeInHierarchy || images.Length <= 0)
+        if (!gameObject.activeInHierarchy || images.Length <= 0 || childRectTransformList.Count <= 0)
         {
             return;
         }
 
-        for(int i = 0;i < images.Length;i++)
+        foreach(var item in childRectTransformList)
+        {
+            Vector3 distanceVector = item.localPosition - uiCenterPoint;
+            
+        }
+
+    }
+
+    public void ModifyImagesInItem(Vector3 distanceVector, float offsetX, float offsetY)
+    {
+        VertexHelper vh = new VertexHelper();
+        for (int i = 0; i < images.Length; i++)
         {
             Graphic graphic = images[i];
+            vh.Clear();
             graphic.OnPopulateMesh_Public(vh);
 
-            rectTransform = GetComponent<RectTransform>();
             var vertexs = new List<UIVertex>();
             vh.GetUIVertexStream(vertexs);
 
             UIVertex vt;
 
-            Vector2 centerPoint;
-            try
-            {
-                RectTransformUtility.ScreenPointToLocalPointInRectangle(graphic.rectTransform, Camera.main.WorldToScreenPoint(new Vector3(centerX, 0, 0)), graphic.canvas.worldCamera, out centerPoint);
-            }
-            catch (Exception e)
-            {
-                print(e);
-                return;
-            }
-
-            _offset = offset;
-            if (centerPoint.x <= 0)
-            {
-                _offset = -offset;
-            }
-
             if (posY == PosY.down)
             {
                 vt = vertexs[0];
-                vt.position = new Vector3((centerPoint.x - vt.position.x) / 10f * perspective + offset, 0, 0);
+                vt.position += new Vector3(offsetX, offsetY, 0);
                 vh.SetUIVertex(vt, 0);
 
                 vt = vertexs[1];
-                vt.position += new Vector3(vt.position.x + offset, vt.position.y, vt.position.z);
+                vt.position += new Vector3(offsetX, offsetY, 0);
                 vh.SetUIVertex(vt, 1);
 
                 vt = vertexs[2];
-                vt.position += new Vector3(vt.position.x + offset, vt.position.y, vt.position.z);
+                vt.position += new Vector3(offsetX, offsetY, 0);
                 vh.SetUIVertex(vt, 2);
 
                 vt = vertexs[4];
-                vt.position = new Vector3((centerPoint.x - vt.position.x) / 10f * perspective + offset, 0, 0);
-                vh.SetUIVertex(vt, 3);
-            }
-            else if (posY == PosY.middle)
-            {
-                vt = vertexs[0];
-                vt.position += new Vector3((centerPoint.x - vt.position.x - vt.position.y) / 10f * perspective + offset, 0, 0);
-                vh.SetUIVertex(vt, 0);
-
-                vt = vertexs[1];
-                vt.position += new Vector3((centerPoint.x + vt.position.x - vt.position.y) / 10f * perspective + offset, 0, 0);
-                vh.SetUIVertex(vt, 1);
-
-                vt = vertexs[2];
-                vt.position += new Vector3((centerPoint.x + vt.position.x - vt.position.y) / 10f * perspective + offset, 0, 0);
-                vh.SetUIVertex(vt, 2);
-
-                vt = vertexs[4];
-                vt.position += new Vector3((centerPoint.x - vt.position.x - vt.position.y) / 10f * perspective + offset, 0, 0);
+                vt.position += new Vector3(offsetX, offsetY, 0);
                 vh.SetUIVertex(vt, 3);
             }
             else
             {
                 vt = vertexs[0];
-                vt.position = new Vector3((centerPoint.y - vt.position.y) / 100 * (vt.position.x - centerPoint.x) * perspective, vt.position.y, vt.position.z);
-                print(vt.position.x);
-                //OUT.worldPosition.x += (-v.vertex.y - _OffsetX) / 1000 * v.vertex.x * _OffsetPerspective;
-                //OUT.worldPosition.x += (_CenterY - v.vertex.y) / 1000 * (v.vertex.x - _CenterX) * _OffsetX;
+                vt.position += new Vector3(offsetX, offsetY, 0);
                 vh.SetUIVertex(vt, 0);
 
                 vt = vertexs[1];
-                vt.position += new Vector3((centerPoint.x - vt.position.x) / 10f * perspective + _offset, 0, 0);
+                vt.position += new Vector3(offsetX, offsetY, 0);
                 vh.SetUIVertex(vt, 1);
 
                 vt = vertexs[2];
-                vt.position += new Vector3((centerPoint.x - vt.position.x) / 10f * perspective + _offset, 0, 0);
+                vt.position += new Vector3(offsetX, offsetY, 0);
                 vh.SetUIVertex(vt, 2);
 
                 vt = vertexs[4];
-                vt.position = new Vector3((centerPoint.y - vt.position.y) / 100 * (vt.position.x - centerPoint.x) * perspective, vt.position.y, vt.position.z);
-                print(vt.position.x);
+                vt.position += new Vector3(offsetX, offsetY, 0);
                 vh.SetUIVertex(vt, 3);
             }
 
@@ -124,7 +127,19 @@ public class PerspectiveImage : MonoBehaviour
     public enum PosY
     {
         up,
-        middle,
         down
+    }
+
+    private Vector3 WorldToScreenPoint(RectTransform parent, Vector3 worldPoint)
+    {
+        //把直接坐标转为屏幕坐标
+        Vector2 screenPoint = RectTransformUtility.WorldToScreenPoint(Camera.main, worldPoint);
+        //把屏幕坐标转为UGUI坐标
+        Vector2 outVector;
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(parent, screenPoint, Camera.main, out outVector))
+        {
+            return new Vector3(outVector.x, outVector.y, 0);
+        }
+        return new Vector3(outVector.x, outVector.y,0);
     }
 }
