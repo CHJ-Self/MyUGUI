36105fb539f4414b9fe06fc80b2f1dd5e510b310
chenhaijian <1216948100@qq.com>
2021-06-13 20:37:01 +0800

    提交透视滑动列表脚本（基本完成，还有Text未完成）

---
+++ Assets/UnityEngine.UI/UI/MyCode/PerspectiveButton.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Graphic))]
+[RequireComponent(typeof(Button))]
+[RequireComponent(typeof(MeshCollider))]
+public class PerspectiveButton : MonoBehaviour
+{
+    private MeshCollider meshCollider;
+    private Graphic graphic;
+    private Button button;
+
+    private void Start()
+    {
+        graphic = GetComponent<Graphic>();
+        button = GetComponent<Button>();
+        meshCollider = GetComponent<MeshCollider>();
+    }
+
+    public void SetColliderMesh()
+    {
+        Mesh mesh = new Mesh();
+        VertexHelper vertexHelper = new VertexHelper();
+        graphic.OnPopulateMesh_Public(vertexHelper);
+        vertexHelper.FillMesh(mesh);
+        meshCollider.sharedMesh = mesh;
+    }
+
+    private void OnMouseDown()
+    {
+        button.onClick.Invoke();
+    }
+
+    public void OnClick()
+    {
+        print(1);
+    }
+}
--- Assets/UnityEngine.UI/UI/MyCode/PerspectiveScrollRect.cs
+++ Assets/UnityEngine.UI/UI/MyCode/PerspectiveScrollRect.cs
@@ -1,30 +1,131 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-[RequireComponent(typeof(ScrollRect))]
 public class PerspectiveScrollRect : MonoBehaviour
 {
-    private ScrollRect scrollRect;
-    private PerspectiveImage[] perspectiveImages; 
+    /// <summary>
+    /// 中心点，中心点可以设置为不是原点
+    /// </summary>
+    public Vector3 uiCenterPoint = Vector3.zero;
 
-    void Start()
+    /// <summary>
+    /// 图片左边的回拉像素，每距离中心点100个像素往回拉的距离
+    /// </summary>
+    [Range(0,100)]
+    public float perspective_left = 0;
+
+    /// <summary>
+    /// 图片右边的回拉像素，每距离中心点100个像素往回拉的距离
+    /// </summary>
+    [Range(0, 100)]
+    public float perspective_right = 0;
+
+    public ScrollRect scrollRect;
+
+    /// <summary>
+    /// 滑动列表节点
+    /// </summary>
+    private RectTransform scrollRectRectTransform;
+    private List<RectTransform> childRectTransformList = new List<RectTransform>();
+    private RectTransform rectTransform;
+
+    private void Start()
     {
-        scrollRect = GetComponent<ScrollRect>();
-        perspectiveImages = GetComponentsInChildren<PerspectiveImage>();
+        scrollRectRectTransform = scrollRect.GetComponent<RectTransform>();
+        rectTransform = GetComponent<RectTransform>();
+        for(int i = 0;i < transform.childCount;i++)
+        {
+            if(transform.GetChild(i).gameObject.activeInHierarchy)
+            {
+                childRectTransformList.Add(transform.GetChild(i).GetComponent<RectTransform>());
+            }
+        }
         scrollRect.onValueChanged.AddListener(UpdataChilds);
         UpdataChilds(Vector2.zero);
     }
 
-    /// <summary>
-    /// 更新子节点
-    /// </summary>
     void UpdataChilds(Vector2 vector2)
     {
-        foreach(var i in perspectiveImages)
+        ModifyMesh(new VertexHelper());
+    }
+
+    public void ModifyMesh(VertexHelper vh)
+    {
+        if (!gameObject.activeInHierarchy || childRectTransformList.Count <= 0)
         {
-            i.ModifyMesh(new VertexHelper());
+            return;
+        }
+
+        foreach(var item in childRectTransformList)
+        {
+            float offset_left;
+            float offset_right;
+            Vector3 distanceVector = new Vector3(item.localPosition.x - scrollRectRectTransform.sizeDelta.x / 2 + rectTransform.anchoredPosition.x, item.localPosition.y, 0) - uiCenterPoint;
+            //distanceVector.x小于0则证明当前节点在中心点右边，设置的透视左右值需要反过来
+            if (distanceVector.x < 0)
+            {
+                offset_left = -perspective_left * distanceVector.x / 100f;
+                offset_right = -perspective_right * distanceVector.x / 100f;
+            }
+            else
+            {
+                offset_left = -perspective_right * distanceVector.x / 100f;
+                offset_right = -perspective_left * distanceVector.x / 100f;
+            }
+            Image[] images = item.GetComponentsInChildren<Image>();
+            ModifyImagesInItem(offset_left, offset_right, images, item.sizeDelta.y);
+        }
+
+    }
+
+    public void ModifyImagesInItem(float offset_left, float offset_right, Image[] images, float itemHeight)
+    {
+        VertexHelper vh = new VertexHelper();
+        for (int i = 0; i < images.Length; i++)
+        {
+            Graphic graphic = images[i];
+            vh.Clear();
+            graphic.OnPopulateMesh_Public(vh);
+
+            var vertexs = new List<UIVertex>();
+            vh.GetUIVertexStream(vertexs);
+
+            UIVertex vt;
+            float ratio0;
+            float ratio1;
+            float ratio2;
+            float ratio3;
+            float graphicPosY = Mathf.Abs(graphic.rectTransform.localPosition.y);
+
+            vt = vertexs[0];
+            ratio0 = (Mathf.Abs(vt.position.y) + graphicPosY) / itemHeight;
+            vt.position += new Vector3(offset_left * ratio0, 0, 0);
+            vh.SetUIVertex(vt, 0);
+
+            vt = vertexs[1];
+            ratio1 = (Mathf.Abs(vt.position.y) + graphicPosY) / itemHeight;
+            vt.position += new Vector3(offset_left * ratio1, 0, 0);
+            vh.SetUIVertex(vt, 1);
+
+            vt = vertexs[2];
+            ratio2 = (Mathf.Abs(vt.position.y) + graphicPosY) / itemHeight;
+            vt.position += new Vector3(offset_right * ratio2, 0, 0);
+            vh.SetUIVertex(vt, 2);
+
+            vt = vertexs[4];
+            ratio3 = (Mathf.Abs(vt.position.y) + graphicPosY) / itemHeight;
+            vt.position += new Vector3(offset_right * ratio3, 0, 0);
+            vh.SetUIVertex(vt, 3);
+
+            Mesh mesh = new Mesh();
+            vh.FillMesh(mesh);
+            graphic.canvasRenderer.SetMesh(mesh);
+            MeshCollider meshCollider = graphic.GetComponent<MeshCollider>();
+            if(meshCollider != null)
+            {
+                meshCollider.sharedMesh = mesh;
+            }
         }
     }
 }
