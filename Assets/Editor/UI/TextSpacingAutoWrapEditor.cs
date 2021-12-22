e00faa807d1dfaff8bc5fb4291d8fb38495b1f57
chenhaijian <1216948100@qq.com>
2021-12-23 01:50:06 +0800

    初步实现了文本的字间距，并且在Inspector面板增加了对应的属性方便调整，后续还有问题需要修改

---
+++ Assets/Editor/UI/TextSpacingAutoWrapEditor.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UnityEditor.UI
+{
+    [CustomEditor(typeof(TextSpacingAutoWrap), true)]
+    [CanEditMultipleObjects]
+    /// <summary>
+    ///   Custom Editor for the Text Component.
+    ///   Extend this class to write a custom editor for an Text-derived component.
+    /// </summary>
+    public class TextSpacingAutoWrapEditor : GraphicEditor
+    {
+        SerializedProperty m_Text;
+        SerializedProperty m_FontData;
+        SerializedProperty m_characterSpacing;
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            m_Text = serializedObject.FindProperty("m_Text");
+            m_FontData = serializedObject.FindProperty("m_FontData");
+            m_characterSpacing = serializedObject.FindProperty("m_characterSpacing");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            EditorGUILayout.PropertyField(m_Text);
+            EditorGUILayout.PropertyField(m_characterSpacing);
+            EditorGUILayout.PropertyField(m_FontData);
+
+            AppearanceControlsGUI();
+            RaycastControlsGUI();
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        public override bool HasPreviewGUI() { return true; }
+
+        public override void OnPreviewGUI(Rect rect, GUIStyle background)
+        {
+            Text text = target as Text;
+            if (text == null) return;
+
+            Texture texture = text.mainTexture;
+            if (texture == null) return;
+
+            Sprite sprite = Sprite.Create(texture as Texture2D, new Rect(0, 0, texture.width, texture.height), Vector2.zero);
+
+            SpriteDrawUtility.DrawSprite(sprite, rect, new Color(1, 1, 1, 1));
+        }
+    }
+}
--- Assets/UnityEngine.UI/UI/MyCode/TextSpacingAutoWrap.cs
+++ Assets/UnityEngine.UI/UI/MyCode/TextSpacingAutoWrap.cs
@@ -5,6 +5,8 @@ using UnityEngine.UI;
 [AddComponentMenu("UI/Effects/TextSpacing")]
 public class TextSpacingAutoWrap : Text
 {
+    [SerializeField] private float m_characterSpacing = 0f;
+
     protected override void OnPopulateMesh(VertexHelper toFill)
     {
         if (font == null)
@@ -26,7 +28,7 @@ public class TextSpacingAutoWrap : Text
         IList<UIVertex> verts = cachedTextGenerator.verts;
         float unitsPerPixel = 1 / pixelsPerUnit;
         int vertCount = verts.Count;
-
+        print(vertCount);
         // We have no verts to process just return (case 1037923)
         if (vertCount <= 0)
         {
@@ -37,17 +39,27 @@ public class TextSpacingAutoWrap : Text
         Vector2 roundingOffset = new Vector2(verts[0].position.x, verts[0].position.y) * unitsPerPixel;
         roundingOffset = PixelAdjustPoint(roundingOffset) - roundingOffset;
         toFill.Clear();
+        UIVertex[] m_tempVert = new UIVertex[4];
+        Vector3 spacing = new Vector3(5,0,0);
+        Vector3 currentSpacing = Vector3.zero;
         if (roundingOffset != Vector2.zero)
         {
             for (int i = 0; i < vertCount; ++i)
             {
                 int tempVertsIndex = i & 3;
-                m_TempVerts[tempVertsIndex] = verts[i];
-                m_TempVerts[tempVertsIndex].position *= unitsPerPixel;
-                m_TempVerts[tempVertsIndex].position.x += roundingOffset.x;
-                m_TempVerts[tempVertsIndex].position.y += roundingOffset.y;
+                m_tempVert[tempVertsIndex] = verts[i];
+                m_tempVert[tempVertsIndex].position *= unitsPerPixel;
+                m_tempVert[tempVertsIndex].position.x += roundingOffset.x;
+                m_tempVert[tempVertsIndex].position.y += roundingOffset.y;
+
+                if(i % 4 == 0)
+                {
+                    currentSpacing += spacing;
+                }
+                m_tempVert[tempVertsIndex].position += currentSpacing;
+
                 if (tempVertsIndex == 3)
-                    toFill.AddUIVertexQuad(m_TempVerts);
+                    toFill.AddUIVertexQuad(m_tempVert);
             }
         }
         else
@@ -55,14 +67,22 @@ public class TextSpacingAutoWrap : Text
             for (int i = 0; i < vertCount; ++i)
             {
                 int tempVertsIndex = i & 3;
-                m_TempVerts[tempVertsIndex] = verts[i];
-                m_TempVerts[tempVertsIndex].position *= unitsPerPixel;
+                m_tempVert[tempVertsIndex] = verts[i];
+                m_tempVert[tempVertsIndex].position *= unitsPerPixel;
+
+                if (i % 4 == 0)
+                {
+                    currentSpacing += spacing;
+                }
+                m_tempVert[tempVertsIndex].position += currentSpacing;
+
                 if (tempVertsIndex == 3)
-                    toFill.AddUIVertexQuad(m_TempVerts);
+                    toFill.AddUIVertexQuad(m_tempVert);
             }
         }
         */
         #endregion
+
 
         Vector2 extents = rectTransform.rect.size;
         var settings = GetGenerationSettings(extents);
@@ -82,43 +102,54 @@ public class TextSpacingAutoWrap : Text
 
         Vector2 roundingOffset = new Vector2(verts[0].position.x, verts[0].position.y) * unitsPerPixel;
         roundingOffset = PixelAdjustPoint(roundingOffset) - roundingOffset;
-        Debug.Log("roungingOffset: " + roundingOffset);
+        //Debug.Log("roungingOffset: " + roundingOffset);
 
-
+        /*
+        foreach(var vert in verts)
+        {
+            Debug.Log(vert.position);
+        }
+        Debug.Log("---------------------------------------------");
+        */
         //--------------------------------------------------------------------------------
         //TODO 对比一下UGUI的方法和自定义方法输出的vert位置
-
+        //TODO 实现Alignment、LinsSpacing以及Overflow的情况
+        //TODO 看看Text是什么时候更新的Texture
 
         toFill.Clear();
-        Vector3 pos = Vector3.zero;
-        //pos = new Vector3(rectTransform.rect.xMin, rectTransform.rect.yMax - fontSize, 0);
 
-        //print(rectTransform.rect.xMin);
-        //print(rectTransform.rect.yMax);
+        float currentLineTotalWidth = 0f;
+        int lineCount = 1;
+        Vector3 startPos = Vector3.zero;
+        startPos = new Vector3(rectTransform.rect.xMin, rectTransform.rect.yMax - fontSize * lineCount, 0);
+        //定义字间距向量
+        Vector3 characterSpacingVector = new Vector3(m_characterSpacing, 0, 0);
+        Vector3 spacing = Vector3.zero;
 
+        Font mfont = Font.CreateDynamicFontFromOSFont("Arail", fontSize);
+        mfont.RequestCharactersInTexture(text, fontSize);
+        
+        CharacterInfo ch_firstChar;
+        mfont.GetCharacterInfo(text[0], out ch_firstChar);
+        currentLineTotalWidth = ch_firstChar.advance;
+        
         for (int i = 0; i < text.Length; i++)
         {
-            // Get character rendering information from the font
-            CharacterInfo ch;
-
             //TODO 研究动态字体和静态字体的区别
-            Font mfont = Font.CreateDynamicFontFromOSFont("Arail", fontSize);
-            mfont.RequestCharactersInTexture(text, fontSize);
+
+            CharacterInfo ch;
             mfont.GetCharacterInfo(text[i], out ch);
 
             UIVertex[] vertices = new UIVertex[4];
-
             vertices[0] = UIVertex.simpleVert;
             vertices[1] = UIVertex.simpleVert;
             vertices[2] = UIVertex.simpleVert;
             vertices[3] = UIVertex.simpleVert;
 
-            vertices[0].position = pos + new Vector3(ch.minX, ch.maxY, 0);
-            vertices[1].position = pos + new Vector3(ch.maxX, ch.maxY, 0);
-            vertices[2].position = pos + new Vector3(ch.maxX, ch.minY, 0);
-            vertices[3].position = pos + new Vector3(ch.minX, ch.minY, 0);
-
-            //print(vertices[0].position);
+            vertices[0].position = startPos + new Vector3(ch.minX, ch.maxY, 0);
+            vertices[1].position = startPos + new Vector3(ch.maxX, ch.maxY, 0);
+            vertices[2].position = startPos + new Vector3(ch.maxX, ch.minY, 0);
+            vertices[3].position = startPos + new Vector3(ch.minX, ch.minY, 0);
 
             //Vector2 adjustVector = Vector2.zero;
             Vector2 adjustVector = new Vector2(0, 0.00f);
@@ -127,83 +158,41 @@ public class TextSpacingAutoWrap : Text
             vertices[1].uv0 = ch.uvTopRight + adjustVector;
             vertices[2].uv0 = ch.uvBottomRight + adjustVector;
             vertices[3].uv0 = ch.uvBottomLeft + adjustVector;
-            
             /*
-            vertices[0].uv0 = vertices[0].position;
-            vertices[1].uv0 = vertices[1].position;
-            vertices[2].uv0 = vertices[2].position;
-            vertices[3].uv0 = vertices[3].position;
-            */
-            
             Debug.Log(ch.uvTopLeft);
             Debug.Log(ch.uvTopRight);
             Debug.Log(ch.uvBottomRight);
             Debug.Log(ch.uvBottomLeft);
-            
+            */
             vertices[0].color = color;
             vertices[1].color = color;
             vertices[2].color = color;
             vertices[3].color = color;
 
-            // Advance character position
-            pos += new Vector3(ch.advance, 0, 0);
+            if(i + 1 < text.Length)
+            {
+                CharacterInfo next_ch;
+                mfont.GetCharacterInfo(text[i + 1], out next_ch);
+
+                if ((currentLineTotalWidth + next_ch.advance + m_characterSpacing) > rectTransform.sizeDelta.x)
+                {
+                    lineCount++;
+                    currentLineTotalWidth = next_ch.advance;
+                    spacing = Vector3.zero;
+                    startPos = new Vector3(rectTransform.rect.xMin, rectTransform.rect.yMax - fontSize * lineCount, 0) + spacing;
+                }
+                else
+                {
+                    spacing += characterSpacingVector;
+                    startPos += new Vector3(ch.advance, 0, 0) + characterSpacingVector;
+                    currentLineTotalWidth += next_ch.advance + m_characterSpacing;
+                }
+            }
 
             toFill.AddUIVertexQuad(vertices);
         }
 
         m_DisableFontTextureRebuiltCallback = false;
-    }
-
-    void RebuildMesh(string str, ref VertexHelper toFill)
-    {
-        // Generate a mesh for the characters we want to print.
-        //var vertices = new Vector3[str.Length * 4];
-        var triangles = new int[str.Length * 6];
-        var uv = new Vector2[str.Length * 4];
-        Vector3 pos = Vector3.zero;
-        for (int i = 0; i < str.Length; i++)
-        {
-            // Get character rendering information from the font
-            CharacterInfo ch;
-            font.GetCharacterInfo(str[i], out ch);
-
-            UIVertex[] vertices = new UIVertex[4];
-            /*
-            vertices[4 * i + 0] = pos + new Vector3(ch.minX, ch.maxY, 0);
-            vertices[4 * i + 1] = pos + new Vector3(ch.maxX, ch.maxY, 0);
-            vertices[4 * i + 2] = pos + new Vector3(ch.maxX, ch.minY, 0);
-            vertices[4 * i + 3] = pos + new Vector3(ch.minX, ch.minY, 0);
-            
-            uv[4 * i + 0] = ch.uvTopLeft;
-            uv[4 * i + 1] = ch.uvTopRight;
-            uv[4 * i + 2] = ch.uvBottomRight;
-            uv[4 * i + 3] = ch.uvBottomLeft;
-
-            triangles[6 * i + 0] = 4 * i + 0;
-            triangles[6 * i + 1] = 4 * i + 1;
-            triangles[6 * i + 2] = 4 * i + 2;
-
-            triangles[6 * i + 3] = 4 * i + 0;
-            triangles[6 * i + 4] = 4 * i + 2;
-            triangles[6 * i + 5] = 4 * i + 3;
-            */
-
-            vertices[0].position = pos + new Vector3(ch.minX, ch.maxY, 0);
-            vertices[1].position = pos + new Vector3(ch.maxX, ch.maxY, 0);
-            vertices[2].position = pos + new Vector3(ch.maxX, ch.minY, 0);
-            vertices[3].position = pos + new Vector3(ch.minX, ch.minY, 0);
-            /*
-            vertices[4 * i + 0].uv0 = ch.uvTopLeft;
-            vertices[4 * i + 1] = ch.uvTopRight;
-            vertices[4 * i + 2] = ch.uvBottomRight;
-            vertices[4 * i + 3] = ch.uvBottomLeft;
-            */
-
-            // Advance character position
-            pos += new Vector3(ch.advance, 0, 0);
-
-            toFill.AddUIVertexQuad(vertices);
-        }
     }
 }
 
